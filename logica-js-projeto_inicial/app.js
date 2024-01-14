100d2d4350b8dca5506c3bb6dad33aad24e4d1a3
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-01-14 09:37:56 -0300

    aula 01

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,27 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
+
+// /**
+//  * Desafio 1
+//  */
+
+// let nome = 'Lua';
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+// let mensagemErro = 'Erro! Preencha todos os campos';
+
+// alert('Boas vindas ao nosso site!');
+// alert(mensagemErro);
+
+// nome = prompt('Informe seu nome');
+// idade = prompt('Informe sua idade');
+
+// if(idade >= 18){
+//     alert('Pode tirar a habilitação!');
+// }
