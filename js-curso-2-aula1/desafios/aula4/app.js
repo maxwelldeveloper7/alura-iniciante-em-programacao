84e4876e3e99ef48b56bc82ddd269e38a9ff0b67
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-01-17 20:58:25 -0300

    aula 4

---
+++ js-curso-2-aula1/desafios/aula4/app.js
@@ -0,0 +1,11 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+let nomes = ['Maxwell', 'Henrique', 'Leonardo'];
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[nomes.length -1]);
--- js-curso-2-aula1/js-curso-2/app.js
+++ js-curso-2-aula1/js-curso-2/app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -34,7 +36,20 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhdo =  parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhdo)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhdo);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhdo;
+    }
 }
 
 function limparCampo(){
